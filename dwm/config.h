cc327e2c66a792886429df1c429fe104ab11eb45
pml68 <pml31@protonmail.com>
2024-02-10 12:43:56 +0100

    feat: add shiftviewclients patch, remove unused tagkeys

--- dwm/config.h
+++ dwm/config.h
@@ -101,6 +101,8 @@ static const Key keys[] = {
     {MODKEY, XK_l, setmfact, {.f = +0.05}},
     {MODKEY, XK_Return, zoom, {0}},
     {MODKEY, XK_Tab, view, {0}},
+    {MODKEY | ShiftMask,  XK_Tab, shiftviewclients, {.i = +1}},
+    {MODKEY | ShiftMask,  XK_7, shiftviewclients, {.i = -1}},
     {MODKEY | ShiftMask, XK_c, killclient, {0}},
     {MODKEY, XK_t, setlayout, {.v = &layouts[0]}},
     {MODKEY, XK_f, setlayout, {.v = &layouts[1]}},
@@ -114,8 +116,7 @@ static const Key keys[] = {
     {MODKEY | ShiftMask, XK_comma, tagmon, {.i = -1}},
     {MODKEY | ShiftMask, XK_period, tagmon, {.i = +1}},
     TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3)
-        TAGKEYS(XK_5, 4) TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7)
-            TAGKEYS(XK_9, 8){MODKEY | ShiftMask, XK_q, quit, {0}},
+        TAGKEYS(XK_5, 4) TAGKEYS(XK_6, 5){MODKEY | ShiftMask, XK_q, quit, {0}},
     {0, XK_Print, spawn, {.v = prtscrcmd}},
     {MODKEY | ShiftMask, XK_h, spawn, {.v = kblayoutcmd}},
     {MODKEY | ShiftMask, XK_f, spawn, {.v = firefoxcmd}},
